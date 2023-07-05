87bcaab8559e2fac2222bfb3c775ca59bad6f283
fanselmo <agenciapenseglobal@gmail.com>
2023-07-05 14:48:46 -0300

    1st commit Agenda Modulo 8

---
+++ main.js
@@ -0,0 +1,38 @@
+const form = document.getElementById('form-contatos')
+const nome = []
+const telefone = []
+
+let linhas = ''
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+
+    adicionaLinha()
+    atualizaTabela()
+
+})
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato')
+    const inputTelefoneContato = document.getElementById('telefone-contato')
+
+    nome.push(inputNomeContato.value)
+    telefone.push(inputTelefoneContato.value)
+
+    let linha = '<tr>'
+    linha += `<td>${inputNomeContato.value}</td>`
+    linha += `<td>${inputTelefoneContato.value}</td>`
+    linha += `</tr>`
+
+    linhas += linha 
+
+    
+        inputNomeContato.value = ''
+        inputTelefoneContato.value = ''
+    
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
